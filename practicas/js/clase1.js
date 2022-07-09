a7799207b7f27972c798c5de8fdc5647c2562d6d
Joshua Candia <joshuacandia74@gmail.com>
2022-07-09 14:50:09 -0300

    conosole log

--- practicas/js/clase1.js
+++ practicas/js/clase1.js
@@ -1,1 +1,13 @@
+let edad = 22; //numero entero//
+const nombre = "Joshua"; //string(los string van entre "")//
+const direccion = "San Juan 915";//string//
+
+//Operaciones//
+
+// Suma(+) Resta(-) Dividir(/) Multiplicar (*)
+
+let num1 = 12
+let num2= 21
+console.log(num1 + num2) //ver el codigo en consola//
+
 
