549e1f3046b242912170723e45aaa0b561022409
Joshua Candia <joshuacandia74@gmail.com>
2022-07-09 15:36:04 -0300

    first activity

--- practicas/js/clase1.js
+++ practicas/js/clase1.js
@@ -6,8 +6,20 @@ const direccion = "San Juan 915";//string//
 
 // Suma(+) Resta(-) Dividir(/) Multiplicar (*)
 
-let num1 = 12
-let num2= 21
-console.log(num1 + num2) //ver el codigo en consola//
+let num1 = 2000
+let num2= 3500
+
+//ver el codigo en consola//
+//alert(num1 + num2)//mensaje en pagina//
+console.log(num1 + num2) 
+console.log("bienvenidos a nuestra app web") //mensaje en consola//
+
+//aca muestro la suma de la variable en la consola
+let numero1 =53;
+let numero2 =27;
+let suma = numero1 + numero2;
+console.log("La suma de los números es:" +suma);
 
 
+//let nombrenuevo= prompt("ingrese su nombre") //promt pide datos al usuario//
+//console.log(nombrenuevo)
---
+++ practicas/js/practica.js
@@ -0,0 +1,6 @@
+//Actividad//
+
+let firstname= prompt("ingrese su nombre")
+let lastname = prompt("ingrese su apellido")
+alert  ("!Hola " + firstname+ " " + lastname +" bienvenido! =)")
+console.log( firstname+" " + lastname)
